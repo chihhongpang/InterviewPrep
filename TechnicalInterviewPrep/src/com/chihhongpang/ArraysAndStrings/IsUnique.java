7ed68d25f631cd137cdb8dfdc3fe1c9c65591014
Chih-Hong Pang <chihhong.pang@gmail.com>
2016-02-16 08:39:33 -0500

    Add solution to IsUnique in ArraysAndStrings.

--- TechnicalInterviewPrep/src/com/chihhongpang/ArraysAndStrings/IsUnique.java
+++ TechnicalInterviewPrep/src/com/chihhongpang/ArraysAndStrings/IsUnique.java
@@ -1,14 +1,25 @@
 package com.chihhongpang.ArraysAndStrings;
 
 public class IsUnique {
-
-	public IsUnique() {
-		// TODO Auto-generated constructor stub
+	
+	//Time O(N), Space O(1)
+	public static boolean isUnique(String input) {
+		if (input.length() > 256) return false;
+		
+		boolean[] charCheck = new boolean[256];
+		for (int i = 0; i < input.length(); i++) { // O(N)
+			int currentCharVal = input.charAt(i);
+			if (charCheck[currentCharVal]) return false;
+			charCheck[currentCharVal] = true;
+		}
+		return true;
 	}
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//First, ask if it's ASCII or Unicode. Assume it's ASCII. Unicode needs more storage.
+		//Usually it's 128 characters for ASCII, or 256 for extended ASCII.
+		System.out.println(isUnique("Hello"));
+		System.out.println(isUnique("abcdefghi"));
 	}
-
+	
 }
