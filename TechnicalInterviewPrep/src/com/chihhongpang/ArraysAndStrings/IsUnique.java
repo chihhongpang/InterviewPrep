edfac53ec198e9352929364554a8e09dbaddfc89
Chih-Hong Pang <chihhong.pang@gmail.com>
2016-02-16 08:23:44 -0500

    Add Eclipse project for the code.

---
+++ TechnicalInterviewPrep/src/com/chihhongpang/ArraysAndStrings/IsUnique.java
@@ -0,0 +1,14 @@
+package com.chihhongpang.ArraysAndStrings;
+
+public class IsUnique {
+
+	public IsUnique() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
