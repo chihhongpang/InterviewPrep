4fbdbfbca346cda7d09c839203e0631b63e20a96
Chih-Hong Pang <chihhong.pang@gmail.com>
2016-02-18 09:54:10 -0500

    Add TreesAndGraph package and pseudocode of BFS and DFS.

---
+++ TechnicalInterviewPrep/src/com/chihhongpang/TreesAndGraphs/BFS.java
@@ -0,0 +1,28 @@
+package com.chihhongpang.TreesAndGraphs;
+
+public class BFS {
+
+	// Pseudocode for the basic BFS for graphs
+//	void search(Node root) {
+//		if (root == null) return;
+//		initialize a queue;
+//		queue.enqueue(root);
+//		
+//		while (queue is not empty) {
+//			Node node = queue.dequeue;
+//			visit(node);
+//			mark the node (already visited or in the queue);
+//			for each neighbor of the node {
+//				if neighbor has not been marked
+//					queue.enqueue(neighbor)
+//					mark the neighbor
+//			}
+//		}
+//	}
+
+	
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ TechnicalInterviewPrep/src/com/chihhongpang/TreesAndGraphs/DFS.java
@@ -0,0 +1,21 @@
+package com.chihhongpang.TreesAndGraphs;
+
+public class DFS {
+
+	// Pseudocode for the basic DFS for graphs
+//	void search(Node root) {
+//		if (root == null) return;
+//		visit(root);
+//		mark root visited;
+//		for each neighbor of root {
+//			if neighbor has not been marked as visited yet,
+//				search(neighbor);
+//		}
+//		
+//	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
