c9a520b5e1db04db6af2ecce6bac3f68e3167ad4
Chih-Hong Pang <chihhong.pang@gmail.com>
2016-02-18 14:56:41 -0500

    Add basic optimized version of QuickSort.

--- TechnicalInterviewPrep/src/com/chihhongpang/SortingAndSearching/QuickSort.java
+++ TechnicalInterviewPrep/src/com/chihhongpang/SortingAndSearching/QuickSort.java
@@ -2,9 +2,47 @@ package com.chihhongpang.SortingAndSearching;
 
 public class QuickSort {
 
+	public static void quickSort(int[] arr) {
+		quickSort(arr, 0, arr.length-1);
+	}
+	
+	private static void quickSort(int[] arr, int left, int right) {
+		int index = partition(arr, left, right);
+		
+		if (left < index -1) quickSort(arr, left, index-1);
+		if (right > index) quickSort(arr, index, right);
+	}
+	
+	private static int partition(int[] arr, int left, int right) {
+		//pick a pivot point
+		int mid = (left + right) / 2;
+		int pivot = arr[mid];
+		
+		while (left <= right) {
+			while (arr[left] < pivot) left++;
+			while (arr[right] > pivot) right--;
+			if (left <= right) {
+				swap(arr, left, right);
+				left++;
+				right--;
+			}
+		}
+		return left;
+	}
+	
+	private static void swap(int[] arr, int left, int right) {
+		int leftCopy = arr[left];
+		arr[left] = arr[right];
+		arr[right] = leftCopy;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] arr = {4,6,7,1,2,3,8,0};
+		quickSort(arr);
+		for (int element: arr) {
+			System.out.println(element);
+		}
+		
 	}
 
 }
