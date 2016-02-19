d16322e947469ed49fd8287771640df308e1834e
Chih-Hong Pang <chihhong.pang@gmail.com>
2016-02-19 07:39:54 -0500

    Add Amazon's 14 Leadership Principles.

---
+++ TechnicalInterviewPrep/src/com/chihhongpang/AmazonLeadershipPrinciple/LeadershipPrinciple.java
@@ -0,0 +1,17 @@
+package com.chihhongpang.AmazonLeadershipPrinciple;
+
+public class LeadershipPrinciple {
+
+	/**Amazon's 14 Leadership Principles:
+	 * Customer Obsession, Ownership, Invent and Simplify, Are right a lot, Hire and Develop the Best,
+	 * Insist on the Highest Standard, Think Big, Bias for Action, Frugality, Learn and Be Curious,
+	 * Earn Trust, Dive Deep, Have Backbone; Disagree and Commit, Deliver Results
+	 * 
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+
+	}
+
+}
