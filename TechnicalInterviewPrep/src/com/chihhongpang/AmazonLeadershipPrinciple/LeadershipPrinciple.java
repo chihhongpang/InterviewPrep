fa6faa21f87ad5412e676ec2b15e1575ce584555
Chih-Hong Pang <chihhong.pang@gmail.com>
2016-02-19 08:29:25 -0500

    Add memorization tip for Amazon's 14 leadership principle.

--- TechnicalInterviewPrep/src/com/chihhongpang/AmazonLeadershipPrinciple/LeadershipPrinciple.java
+++ TechnicalInterviewPrep/src/com/chihhongpang/AmazonLeadershipPrinciple/LeadershipPrinciple.java
@@ -7,6 +7,16 @@ public class LeadershipPrinciple {
 	 * Insist on the Highest Standard, Think Big, Bias for Action, Frugality, Learn and Be Curious,
 	 * Earn Trust, Dive Deep, Have Backbone; Disagree and Commit, Deliver Results
 	 * 
+	 * Found from Amazon's website: https://www.amazon.jobs/en/principles
+	 * 
+	 * 
+	 * Memorization tip: make it like a story
+	 * COIAH ITBFL EDHD
+	 * 
+	 * >> BIO DITCHED HALF
+	 * Bias on Action, Invent and Simplify, Ownership
+	 * Dive Deep, Insist on Highest Standard, Think Big, Customer Obsession, Have Backbone; Disagree and Commit, Earn Trust, Deliver Results
+	 * Hire and Develop the Best, Are right a lot, Learn and Be Curious, Frugality
 	 * 
 	 */
 	
